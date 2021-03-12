0f8ef34f013fad854b45f8d35b1738a2379ae06f
jwen-adobe <jwen@adobe.com>
2021-03-11 21:39:06 -0800

    test

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -12,7 +12,7 @@ async function readLines(stream) {
     return new Promise(resolve => {
         stream.once('error', _ => resolve(null));
         const lines = [];
-        rl.on('line', line => lines.push(line));
+        rl.on('line', line => lines.push(line.replace(",","")));
         rl.on('close', _ => resolve(lines));
     });
 }
@@ -20,10 +20,29 @@ async function readLines(stream) {
 readLines(fs.createReadStream("listOfXdms.txt")).then(result => {
     let obj = {};
     for (let i = 0; i < result.length; i++) {
-        dotProp.set(obj, result[i], result[i].split(".").pop());
+        dotProp.set(obj, result[i], result[i]);
         //console.log(result[i])
     }
-    console.log(JSON.stringify(obj, null,2))
+    fs.unlinkSync("prod/master/dropdowntest.md");
+    fs.appendFileSync("prod/master/dropdowntest.md", "# XDM Visualization\n", 'utf8');
+    fs.appendFileSync("prod/master/dropdowntest.md", "## Git Repo Branch: master\n", 'utf8');
+    dropDownGen(obj)
 })
 
+function dropDownGen(o) {
+    //console.log(JSON.stringify(o, null,2))
+    for (let i in o) {
+        if (o[i] !== null && typeof(o[i]) == "object") {
+            fs.appendFileSync("prod/master/dropdowntest.md", "<details>\n", 'utf8');
+            fs.appendFileSync("prod/master/dropdowntest.md", "<summary>" + i + "</summary>\n", 'utf8');
+            fs.appendFileSync("prod/master/dropdowntest.md", "<ul>\n", 'utf8');
+            dropDownGen(o[i]);
+            fs.appendFileSync("prod/master/dropdowntest.md", "</ul>\n", 'utf8');
+            fs.appendFileSync("prod/master/dropdowntest.md", "</details>\n", 'utf8');
+        }
+        else {//leaf
+            fs.appendFileSync("prod/master/dropdowntest.md", "<li>"+o[i]+"</li>\n", 'utf8');
+        }
+    }
 
+}
